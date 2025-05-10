02ae82013e6b7942297effb23cd82cf0fbc83e04
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-10 12:50:02 +0530

    Import user components into App component, including Login and Register, to facilitate user authentication and registration processes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import ProtectedRoute from './components/ProtectedRoute';
+
+// User components
+import Login from './pages/user/Login';
+import Register from './pages/user/Register';
