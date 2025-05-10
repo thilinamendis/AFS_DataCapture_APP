38b37880bca22c864b96dd925ac134b220327ae3
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-10 15:00:13 +0530

    Add user model: implement user schema with fields for firstname, lastname, email, password, phone, address, userType, and isAdmin, including timestamps for better data management.

---
+++ server/model/userModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: false,
+        default: '',
+    },
+    address: {
+        type: String,
+        required: false,
+        default: '',
+    },
+    userType: {
+        type: String,
+        enum: ['admin', 'technician'],
+        default: 'technician',
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
