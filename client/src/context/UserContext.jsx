b59187558e5572d247c6cb90be95ce26e7ecae37
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-10 10:32:22 +0530

    Add useUser hook to UserContext for improved user state management

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -140,7 +140,13 @@ export const UseProvider = ({Children})=>{
         }}>
             {Children}
         </useContext.Provider>
-    )
+    );
+};
 
-
-}
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (!context) {
+        throw new Error('useUser must be used within a UserProvider');
+    }
+    return context;
+}; 
