ac0db0d645a07cddde65650938d60340eb520945
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-10 15:00:22 +0530

    Refactor authRoutes: update import paths for express and controllers, and improve code formatting for better readability and consistency.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,27 +1,26 @@
-import expres from 'express';
-import { login, registerUser, getUser, updateUser, deleteUser, getUsers, createUserByAdmin, getUserById, updateUserByAdmin, deleteUserByAdmin } from '../controllers/AuthController';
-import { protect } from '../Middleware/AuthMiddleware';
+import express from 'express';
+import { login, registerUser, getUser, updateUser, deleteUser, getUsers, createUserByAdmin, getUserById, updateUserByAdmin, deleteUserByAdmin } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
-
-const router = expres.Router();
+const router = express.Router();
 
 //Public routes
-router.post('/register',registerUser);
-router.post('/login',login);
+router.post('/register', registerUser);
+router.post('/login', login);
 
 //protected routes
-router.get('/me',protect,getUser);
-router.put('/update',protect,updateUser);
-router.delete('/delete',protect,deleteUser);
+router.get('/me', protect, getUser);
+router.put('/update', protect, updateUser);
+router.delete('/delete', protect, deleteUser);
 
 //User management routes
 router.route('/users')
-    .get(protect,getUsers)
-    .post(protect,createUserByAdmin);
+    .get(protect, getUsers)
+    .post(protect, createUserByAdmin);
 
 router.route('/users/:id')
-    .get(protect,getUserById)
-    .put(protect,updateUserByAdmin)
-    .delete(protect,deleteUserByAdmin);
+    .get(protect, getUserById)
+    .put(protect, updateUserByAdmin)
+    .delete(protect, deleteUserByAdmin);
 
 export default router;
