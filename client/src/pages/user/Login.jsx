78bf0a4a2b0dde4704ea427116858f9771f48922
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-10 14:58:56 +0530

    Remove school logo from Login component to streamline the user interface.

--- client/src/pages/user/Login.jsx
+++ client/src/pages/user/Login.jsx
@@ -72,11 +72,6 @@ function Login() {
     return(
         <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white flex flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <img
-                    className="mx-auto h-12 w-auto"
-                    src={schoolLogo}
-                    alt="School Logo"
-                />
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                     Sign in to your account
                 </h2>
