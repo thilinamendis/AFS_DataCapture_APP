bf93a9bf9bf24c946ad6690879ced3d11ae37d5a
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-10 12:49:34 +0530

    Add routing and user context to App component, setting up the foundation for protected routes and user management in the application.

---
+++ client/src/App.jsx
@@ -0,0 +1,3 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import ProtectedRoute from './components/ProtectedRoute';
