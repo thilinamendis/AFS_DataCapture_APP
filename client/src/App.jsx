6909504f0da9a97668e6fd249d4b54ca7b052673
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-10 12:50:28 +0530

    Add admin components to App component, including AdminLayout, AdminDashboard, and UserManagement, to support administrative functionalities and user management features.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,3 +5,8 @@ import ProtectedRoute from './components/ProtectedRoute';
 // User components
 import Login from './pages/user/Login';
 import Register from './pages/user/Register';
+
+// Admin components
+import AdminLayout from './pages/admin/AdminLayout';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import UserManagement from './pages/admin/UserManagement';
