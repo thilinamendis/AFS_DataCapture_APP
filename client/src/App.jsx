8c6d5223ede725e647a5d005f3f5e999a088bdca
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-10 12:55:10 +0530

    Refactor App component to include student routing, adding StudentLayout, StudentDashboard, StudentProfile, StudentExams, and StudentAssignments for enhanced student functionalities and user experience.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,9 +11,44 @@ import AdminLayout from './pages/admin/AdminLayout';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import UserManagement from './pages/admin/UserManagement';
 
-// Student components
-import StudentLayout from './pages/student/StudentLayout';
-import StudentDashboard from './pages/student/StudentDashboard';
-import StudentProfile from './pages/student/StudentProfile';
-import StudentExams from './pages/student/StudentExams';
-import StudentAssignments from './pages/student/StudentAssignments';
+// // Student components
+// import StudentLayout from './pages/student/StudentLayout';
+// import StudentDashboard from './pages/student/StudentDashboard';
+// import StudentProfile from './pages/student/StudentProfile';
+// import StudentExams from './pages/student/StudentExams';
+// import StudentAssignments from './pages/student/StudentAssignments';
+
+function App() {
+  return (
+    <Router>
+      <UserProvider>
+        <Routes>
+          {/* Root path redirects to login */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Admin Routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="users" element={<UserManagement />} />
+          </Route>
+
+          {/* Student Routes */}
+          <Route path="/student/*" element={
+            <StudentLayout />
+          }>
+            <Route index element={<StudentDashboard />} />
+            <Route path="profile" element={<StudentProfile />} />
+            <Route path="exams" element={<StudentExams />} />
+            <Route path="assignments" element={<StudentAssignments />} />
+          </Route>
+        </Routes>
+      </UserProvider>
+    </Router>
+  );
+}
+
+export default App;
